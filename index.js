a4878d974796cc7114bab40181ff425f3b9f39bf
saac03 <isaac.dicayanan@gmail.com>
2024-02-16 03:33:25 +0800

    Added admin view

--- index.js
+++ index.js
@@ -16,8 +16,24 @@ function closeSignupForm() {
 
 function redirectToUserOrAdmin() {
     if (document.getElementById("login-admin").checked) {
-        window.location.href = "test.html"; // pa change na langg
+        window.location.href = "admin.html";
     } else if (document.getElementById("login-user").checked) {
         window.location.href = "test.html"; // same here
     }
+}
+
+function logout(){
+    window.location.href = "index.html"
+}
+
+function removeRestaurant(){
+    window.location.href = "removeRestaurant.html" // placeholder
+}
+
+function editRestaurantInfo(){
+    window.location.href="editRestaurant.html"
+}
+
+function addRestaurant(){
+    window.location.href="addRestaurant.html";
 }
