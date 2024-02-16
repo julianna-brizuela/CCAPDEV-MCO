76e0e30d0847f03762b2359244569bd070b7e1ca
julianna-brizuela <julianna_brizuela@dlsu.edu.ph>
2024-02-16 21:32:23 +0800

    Changed the navigation bar and added JS filters

--- filters.js
+++ filters.js
@@ -1,15 +1,14 @@
-function filterByPrice() {
+function filterBySearch() {
+    //solution from: https://stackoverflow.com/questions/43821938/search-div-for-text
+    var input = document.getElementById("search-bar");
+    var filter = input.value.toLowerCase();
+    var restaurants = document.getElementsByClassName("restaurant");
 
-}
-
-function filterByTag() {
-    
-}
-
-function filterByStars() {
-    
-}
-
-function filterBySearch(input) {
-    
+    for (i = 0; i < restaurants.length; i++) {
+        if (restaurants[i].innerText.toLowerCase().includes(filter)) {
+            restaurants[i].style.display = "flex";
+        } else {
+            restaurants[i].style.display = "none";
+        }
+    }
 }
