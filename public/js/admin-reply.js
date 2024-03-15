91344478961de741bbc25e3d59d09562af9e331e
saac03 <isaac.dicayanan@gmail.com>
2024-03-15 13:21:41 +0800

    admin reply works

--- public/js/admin-reply.js
+++ public/js/admin-reply.js
@@ -1,23 +1,83 @@
-// import database from '../../../db/database.js'
-let documents;
-console.log("wwwwwwwwww");
+const reviewForm = document.forms.reviewForm;
 
-function reply(reviewNum){
+async function reply(reviewNum){
     var descriptionID = "review-description-" + reviewNum; 
+    var reviewerID = "reviewer-name-" + reviewNum;
     var responseID = "owner-response-" + reviewNum;
+    var dateID = "date-of-review-" + reviewNum;
+    var ratingID = "rating-" + reviewNum;
 
+    var restaurant = document.getElementById("restaurant-name").innerHTML;
+    var reviewer = document.getElementById(reviewerID).innerHTML;
+    var rating = document.getElementById(ratingID).innerHTML;
+    var responseDate = document.getElementById(dateID).innerHTML;
     var description = document.getElementById(descriptionID).innerHTML;
-    var responseId = document.getElementById(responseID).value;
-    
-    
-    console.log('\n\n---------- UPDATE MANY ----------');
+    var ownerResponse = document.getElementById(responseID).value;
 
-    // database.collections['users'].updateOne({ name: 'JOSH HUTCHERSON' }, { name: 'Josh Hutcherson' });
-    // documents = database.collections['users'].find({ name: 'Josh Hutcherson' });
-    // console.log(documents);
+ 
+    let data = new FormData(reviewForm);
+    data = Object.fromEntries(data);
 
-    console.log(description);
-    console.log(responseId);
-    
+    data['restaurant'] = restaurant;
+    data['reviewer_name'] = reviewer;
+    data['review_rating'] = rating;
+    data['date_of_review'] = responseDate;
+    data['review_description'] = description;
+    data['owner_response'] = ownerResponse;
+
+    // console.log(restaurant);
+    // console.log(reviewer);
+    // console.log(rating);
+    // console.log(responseDate);
+    // console.log(description);
+    // console.log(ownerResponse);
+    const objectOrder = {
+        'restaurant': null,
+        'reviewer_name': null,
+        'review_rating': null,
+        'date_of_review': null,
+        'review_description': null,
+        'owner_response': null
+    }
+
+    data = Object.assign(objectOrder, data);
+    const jstring = JSON.stringify(data); 
+
+    console.log(data);
+    console.log("jstring:");
+    console.log(jstring);
+    try {
+        const review = await fetch("/admin/:username", {
+            method: 'POST',
+            body: jstring,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });    
+        if (review.status == 200) {
+            location.reload(); // refresh the page
+        } else {
+            const message = `An error has occured. Status code: ${review.status}`;
+            alert(message);
+            console.log(message);
+        }
+        
+    } catch (err) {
+        console.error(err);
+        }
 }
+
+// document.addEventListener("DOMContentLoaded", function() {
+//     // Function to handle the button click
+//     function handleClick(event) {
+//         event.preventDefault(); // Prevents the default behavior (page reload) of the button click
+//         console.log("Button clicked!");
+        
+//     }
+
+//     // Attach the handleClick function to the button click event
+//     const submitButton = document.getElementById("button-0");
+//     submitButton.addEventListener("click", handleClick);
+// });
+
 
--- src/routes/adminRouter.js
+++ src/routes/adminRouter.js
@@ -20,8 +20,7 @@ adminRouter.get("/admin", (req, res) => {
      });
  });
 
- adminRouter.get("/admin/:username", (req, res) => {
-
+ adminRouter.get("/admin/:username", async (req, res) => {
     let username = req.params.username;
     username = decodeURIComponent(username);
 
@@ -42,27 +41,64 @@ adminRouter.get("/admin", (req, res) => {
     }
  });
 
+ adminRouter.post("/admin/:username", async (req, res) => {
+    try {
+        const { restaurant, reviewer_name, review_rating, date_of_review, review_description, owner_response } = req.body;
+        console.log("restaurant:", req.body.restaurant);
+        console.log("name:", req.body.reviewer_name);
+        console.log("ratineg:", req.body.review_rating);
+        console.log("date:", req.body.date_of_review);
+        console.log("desc:", req.body.review_description);
+        console.log("response:", req.body.owner_response);
 
-// adminRouter.get("/admin/:username", (req, res) => {
-    
-//     let username = req.params.username;
-//     username = decodeURIComponent(username);
 
-//     const admin = data.admins.find(admin => admin.username === username);
-//     if(admin){
-//         const restaurantReviews = data.reviews.filter(review => review.restaurant === admin.restaurant_name)
-//         const adminRestaurant = data.restaurants.find(restaurant => restaurant.restaurant_name === admin.restaurant_name);
-//         console.log("Hello");
-//         const title = `MUNCH | ${admin.restaurant_name}`;
-//         res.render("admin-restaurant", {
-//             title: title,
-//             reviews: restaurantReviews,
-//             restaurant: adminRestaurant
-//         });
-//     } else {
-//         res.status(404).send('User not found');
+        database.collections['reviews'].updateOne({ reviewer_name: req.body.reviewer_name,
+                                                    restaurant: req.body.restaurant,
+                                                    review_description: req.body.review_description}, { owner_response: req.body.owner_response });
+        documents = database.collections['reviews'].find({ reviewer_name: req.body.reviewer_name,
+                                                            restaurant: req.body.restaurant,
+                                                            review_description: req.body.review_description})[0];
+        console.log('\n\n---------- UPDATE ONE ----------');
+        console.log(documents);
+
+
+    } catch (error) {
+        console.error('Error updating owner response:', error);
+        res.status(500).send('Internal server error');
+    }
+});
+//////////////////////////////////////
+//  restaurantRouter.post('/:restaurant/writeareview', async (req, res) => {
+//     let restaurant_route = req.body.restaurant;
+
+//     function validRestaurant(restaurant) {
+//         let restaurant_name = restaurant.restaurant_name;
+//         return restaurant_name===restaurant_route;
 //     }
-//  });
 
+//     const prev_length = database.collections['reviews'].getLength();
 
+//     let restaurant = database.collections['restaurants'].documents.find(validRestaurant);
+
+//     try {
+//         //inserts new entry into the database
+//         const review = await database.collections['reviews'].insertOne(req.body);
+//         console.log(database.collections['reviews'])
+
+//         //checking if it inserted correctly
+//         if (prev_length < database.collections['reviews'].getLength()) {
+//             res.sendStatus(200);
+//             if (restaurant) {
+//                 const update = await database.collections['restaurants'].updateOne({restaurant_name: restaurant.restaurant_name}, {resto_reviews: database.collections['reviews'].find({restaurant: restaurant.restaurant_name})});
+                
+//             }
+//         } else {
+//             res.sendStatus(500);
+//         }
+//     } catch (err) {
+//         console.error(err);
+//         res.sendStatus(500);
+//     }
+    
+// });
 module.exports = adminRouter;
