9972c78c4f9031bb4f12d5169e9a27aa632d806f
Nash Luis Maramag <09louisthebob@gmail.com>
2024-03-17 03:25:34 +0800

    Changes

--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -3,8 +3,7 @@ module.exports = {
         if (text) {
             text = text.replace(/\s/g, '');
             return text.toLowerCase(); 
-        }
-        
+        }    
     },
     isEqual: function(arg1, arg2) {
         return (arg1 === arg2) ? 1 : 0;
