af6d52471227eff79c2b6dece41c5d8d0bea6bcb
Nash Luis Maramag <09louisthebob@gmail.com>
2024-04-04 12:22:04 +0800

    Minor refactor
    - Cleaned app.js
    - Implemented absolute imports
    - Added hbs-helpers.js and route-handler.js

--- app.js
+++ app.js
@@ -1,60 +1,36 @@
+// Library Imports
 require('dotenv/config');
 const express = require('express');
-const hbs = require('express-handlebars');
+const exphbs = require('express-handlebars');
+
+// Local File Imports
+const helpers = require('#helpers/hbs-helpers.js');
+const { connect } = require('#models/conn.js');
+const routes = require('#routes/route-handler.js')
+
+// Server Setup
+const PORT = process.env.PORT ?? 3000;
 const app = express();
-const PORT = process.env.PORT;
+const hbs = exphbs.create({ extname: 'hbs', helpers });
 
-const { connect } = require('./src/models/conn.js');
-
-// Configurations
-app.use('/static', express.static(__dirname + '/public'));
-// app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.engine('hbs', hbs.engine({
-    extname: 'hbs',
-    helpers: {
-        'toLink': function (text) { 
-            if (text) {
-                text = text.replace(/\s/g, '');
-                return text.toLowerCase(); 
-            }
-            
-        },
-        'isEqual': function(arg1, arg2) {
-            if (arg1 === arg2) { 
-                return 1
-            } else {
-                return 0
-            } 
-        }
-    } 
-}));
+// Server Configurations
+app.engine('hbs', hbs.engine);
 app.set('view engine','hbs');
 app.set('views', './views');
 app.set('view cache', false);
-
-// Routers
-const home = require('./src/routes/home.js');
-const auth = require('./src/routes/auth.js');
-const adminRouter = require('./src/routes/adminRouter.js');
-const indexRouter = require('./src/routes/indexRouter.js');
-const restaurantRouter = require('./src/routes/restaurantsRouter.js');
-const usersRouter = require('./src/routes/usersRouter.js');
-
+app.use('/static', express.static(__dirname + '/public'));
 app.use(express.json());
-app.use('/', home);
-app.use('/', auth);
-app.use('/', adminRouter);
-app.use('/', indexRouter);
-app.use(restaurantRouter);
-app.use(usersRouter);
+
+// Router Configuration
+app.use(routes);
 
 // Server Startup
-app.listen(process.env.PORT, async function() {
-    console.log(`express app is now listening on port ${process.env.PORT}`);
+app.listen(PORT, async () => {
+    console.log(`Express app is now listening on port: ${process.env.PORT}`);
     try {
         await connect();
         console.log(`Now connected to MongoDB`);
+
     } catch (err) {
         console.log('Connection to MongoDB failed: ');
         console.error(err);
